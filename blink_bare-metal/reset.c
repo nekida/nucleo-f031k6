0b86ef914d95641ace26a37a8e1da4993faa2ca2
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2020-12-28 14:38:37 +0300

    Update reset.c

--- blink_bare-metal/reset.c
+++ blink_bare-metal/reset.c
@@ -7,15 +7,15 @@
 #define GPIO_OTYPER_OFFSET	0x4
 #define GPIO_OSPEEDR_OFFSET	0x8
 #define GPIO_BSRR_OFFSET	0x18
-#define PB3_SHIFT			3
+#define PB3_SHIFT		3
 
 
  __attribute__ ((naked)) void reset_handler (void)
 {
-    (*(volatile unsigned int *)(RCC_REGISTER_BASE + RCC_AHBENR_OFFSET)) |= (1 << RCC_IOPBEN_SHIFT);		//clock GPIOB
+    (*(volatile unsigned int *)(RCC_REGISTER_BASE + RCC_AHBENR_OFFSET)) |= (1 << RCC_IOPBEN_SHIFT); //clock GPIOB
 
-    (*(volatile unsigned int *)(AHB2_GPIOB_BASE + GPIO_MODER_OFFSET)) 	|= (0x01 << (PB3_SHIFT * 2));	//PB3 output mode
-    (*(volatile unsigned int *)(AHB2_GPIOB_BASE + GPIO_OSPEEDR_OFFSET)) |= (0x11 << (PB3_SHIFT * 2));	//high speed
+    (*(volatile unsigned int *)(AHB2_GPIOB_BASE + GPIO_MODER_OFFSET)) |= (0x01 << (PB3_SHIFT * 2));   //PB3 output mode
+    (*(volatile unsigned int *)(AHB2_GPIOB_BASE + GPIO_OSPEEDR_OFFSET)) |= (0x11 << (PB3_SHIFT * 2)); //high speed
 
     while(1) {
         (*(volatile unsigned int *)(AHB2_GPIOB_BASE + GPIO_BSRR_OFFSET)) |= (1 << PB3_SHIFT);	//set
